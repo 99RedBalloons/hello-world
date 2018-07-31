51ca919931e99e79bfad15accf7912a83eab3171
99RedBalloons <34426771+99RedBalloons@users.noreply.github.com>
2018-07-31 19:29:55 +0100

    Update HelloWorld.java

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.example.HelloWorld;
 public class HelloWorld {
     public static void main (String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello W!");
         System.out.println("Nice work Rich!");
     }
 }
